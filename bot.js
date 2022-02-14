c03a74d73fc16e95b66502e7d7693a69e586ac5c
Sylvain <sylvain.murati@gmail.com>
2022-02-14 11:57:48 +0100

    ManageChannel vérifie maintenant si tous les channels sont bien en place

--- bot.js
+++ bot.js
@@ -4,6 +4,11 @@ const categoryName="NEW";
 const textChannel ="GUILD_TEXT";
 const config = require('./config.json');
 
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 GetTargetChannelByName = async (Name,FetchRequest) => {
   let listeChannel= await FetchRequest; 
   let Channel = listeChannel.find(chanenel=>chanenel.name==Name);
@@ -23,15 +28,37 @@ GetAllChildren = (categoryChannel,property) =>{
   return ListNameChildren;
 }
 
-CreateAllChilldrenNeed = (listChannel,listNewChannel,categoryChannel)=> {
-  let arrayChildrenName = GetAllChildren(categoryChannel,"name");
-  listNewChannel.forEach(element => {
-    if (!arrayChildrenName.includes(element)){
-      CreateChannel(listChannel,element,categoryChannel.id,textChannel);
-    }else{
-      console.log(element+" existe déjà dans le salon "+categoryName);
+CreateAllChilldrenNeed = (listChannel,listNewChannel,Request)=> {
+  return new Promise(async (resolve)=>{
+    let RequestFetch = await Request.fetch()
+    let CategoryChannel = await GetTargetChannelByName(categoryName,RequestFetch);
+    var numberchildren = CategoryChannel.children.size;
+    let targetNumberChannel=numberchildren
+    
+    let arrayChildrenName = GetAllChildren(CategoryChannel,"name");
+    listNewChannel.forEach(element => {
+      if (!arrayChildrenName.includes(element)){
+        CreateChannel(listChannel,element,CategoryChannel.id,textChannel);
+        targetNumberChannel++
+      }else{
+        console.log(element+" existe déjà dans le salon "+categoryName);
+      }
+    })
+    //console.log(numberchildren,targetNumberChannel)
+    while (numberchildren !== targetNumberChannel) {
+      let RequestFetch = await Request.fetch()
+      let CategoryChannel = await GetTargetChannelByName(categoryName,RequestFetch);
+      var numberchildren = CategoryChannel.children.size;
+      await sleep(1000)
+      
     }
-  });
+    if(numberchildren==targetNumberChannel){
+      resolve()
+    }
+    
+    
+
+  })
 }
 SendAllMessageToChannel = async (ActiveChannel,AllIdchildrenChannel,content,) => {
   let ChannelToSendMessage= await ActiveChannel.fetch(AllIdchildrenChannel[1])
@@ -39,30 +66,24 @@ SendAllMessageToChannel = async (ActiveChannel,AllIdchildrenChannel,content,) =>
   return 0;
 }
 
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+
 async function Main(array,Client){
   
   let serv=Client.guilds.cache;
   let guildID = Client.guilds.cache.keys().next().value;
   let ChannelsClient=Client.channels;
   let Request = serv.get(guildID).channels
-  let RequestFetch = await Request.fetch()
-  
-  let CategoryChannel = await GetTargetChannelByName(categoryName,RequestFetch);
-  
-  
-  CreateAllChilldrenNeed(Request,array,CategoryChannel);
-  await sleep(2000)
-  
-  let refrechChildrenId = GetAllChildren(CategoryChannel,"id");
-  SendAllMessageToChannel(Client.channels,refrechChildrenId ,"C'est un message");
+  let CategoryChannelTarget = await GetTargetChannelByName(categoryName,Request.fetch(),"id")
+
+  await CreateAllChilldrenNeed(Request,array,Request);
+  console.log("Création des channels terminer")
+  let refrechChildrenId = GetAllChildren(CategoryChannelTarget,"id");
+  SendAllMessageToChannel(ChannelsClient,refrechChildrenId ,"C'est un message");
 
 }
 
 bot.on('ready',  () =>{
-  let NewChannel=["coucou","troll"];
+   let NewChannel=["coucou","troll"];
   Main(NewChannel,bot);
 })
 
